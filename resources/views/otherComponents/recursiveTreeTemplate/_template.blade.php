37bf0ef4c010ea9a8b902835a6e7e762d6b831cf
Alex <ghostfirelake@gmail.com>
2018-10-23 16:47:32 +0400

    23.10 commit

--- resources/views/otherComponents/recursiveTreeTemplate/_template.blade.php
+++ resources/views/otherComponents/recursiveTreeTemplate/_template.blade.php
@@ -1,13 +1,13 @@
-<ul class="list-group list-group-flush pl-3">
+<ul class="list-group">
     @foreach ($categories as $category)
-        <li class="list-group-item justify-content-between align-items-center m-0 p-2">
+        <li class="list-group-item justify-content-between m-0 pl-4">
             <a class="text-dark" href="#"> {{ $category->name }} </a>
             <span class="badge badge-primary badge-pill"> {{ $category->children->count() }} </span>
             <div class="">
                 <form method="post" action="{{ route('categories.destroy', $category->id) }}">
                     @csrf
                     {{ method_field('DELETE') }}
-                    <button type="submit" class="btn btn-primary btn-sm" href="{{ route('categories.destroy', $category->id) }}">
+                    <button type="submit" class="btn-sm btn btn-secondary" href="{{ route('categories.destroy', $category->id) }}">
                         <span class="glyphicon glyphicon-trash">Delete this Glif Will</span>
                     </button>
                 </form>
